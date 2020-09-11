7946e2629ada232cf5e38481b1df83e2c53aa500
Santosh Kumar Singh <ss7257711@gmail.com>
2020-09-11 20:03:37 +0530

    Update Day1.py
    Largest Time for Given Digits Problem

--- Day1.py
+++ Day1.py
@@ -1,6 +1,8 @@
 '''
 Question Description :-
 
+Largest Time for Given Digits
+
 Given an array of 4 digits, return the largest 24 hour time that can be made.
 The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:00, a time is larger if more time has elapsed since midnight.
 Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
